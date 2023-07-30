6ade0384d46d1a7325cae05446438a22c123e2f7
fluffysnowman <51316255+FluffySnowman@users.noreply.github.com>
2023-07-31 00:21:09 +0530

    added react app dependency installation option (npm)
    *app startup causes terminal to not register input (commented out)

--- all_generations/generateReactApp.go
+++ all_generations/generateReactApp.go
@@ -2,9 +2,11 @@ package all_generations
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/fluffysnowman/vortexus/gen"
 	"github.com/fluffysnowman/vortexus/templates"
+	"github.com/gookit/color"
 )
 
 
@@ -53,6 +55,75 @@ func GenerateReactApp() {
 	gen.ReadInput()
 	fmt.Printf("\nCreating React App...\n")
 	gen.GenerateFiles(webServerPort, options, fileTemplates)
+
+	fmt.Printf("\n\nReact App created\n")
+
+	color.Yellow.Printf("\nDo you want to install the dependencies for the react app ? (y/n) ")
+	installDependencies := gen.ReadInput()
+
+	if installDependencies == "y" {
+		color.Green.Printf("\nInstalling dependencies...\n\n")
+
+		installDependenciesCommandWindows := exec.Command("cmd", "/C", "cd", "react_app", "&&", "npm", "install")
+		out, err := installDependenciesCommandWindows.Output()
+
+		if err != nil {
+
+			installDependenciesCommandLinux := exec.Command("sh", "-c", "cd react_app && npm install")
+			out, err = installDependenciesCommandLinux.Output()
+
+			if err != nil {
+				fmt.Println("Error installing dependencies: ", err)
+			}
+		}
+
+		fmt.Println(string(out))
+		color.Green.Printf("\nDependencies installed\n\n")
+	} else {
+		fmt.Printf("\nSkipping dependency installation (you can do this manually by running \"npm install\" in the react_app directory\n")
+	}
+
+/* 
+	color.Yellow.Printf("\nDo you want to start the react app (y/n) ")
+	startReactApp := gen.ReadInput()
+
+	if startReactApp == "y" {
+    color.Green.Printf("\nStarting react app...\n\n")
+
+    var startReactAppCommand *exec.Cmd
+    if runtime.GOOS == "windows" {
+        startReactAppCommand = exec.Command("cmd", "/C", "cd", "react_app", "&&", "npm", "start")
+    } else {
+        startReactAppCommand = exec.Command("sh", "-c", "cd react_app && npm start")
+    }
+
+    startReactAppCommand.Stdout = os.Stdout
+    startReactAppCommand.Stderr = os.Stderr
+
+    startReactAppCommand.Stdin = os.Stdin
+
+    // start the app async
+    err := startReactAppCommand.Start()
+    if err != nil {
+        fmt.Println("Error starting react app: ", err)
+        return
+    }
+
+    //  wait for the cmd to fininsh
+    err = startReactAppCommand.Wait()
+    if err != nil {
+        fmt.Println("Error while running react app: ", err)
+    }
+
+    color.Green.Printf("\nReact app finished\n\n")
+	} else {
+		fmt.Printf("\nSkipping react app start (you can do this manually by running \"npm start\" in the react_app directory\n")
+	}
+ */
+
+
+	color.Green.Printf("To run the react app, run \"cd react_app && npm start\"\n\n")
+
 
 }
 
